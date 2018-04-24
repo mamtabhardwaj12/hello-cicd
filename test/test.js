765b3284fe01a2acd9371dd9a773ed7aef527fcf
mamtabhardwaj12 <mamta@cateina.com>
2018-04-24 19:46:39 +0530

    Switch to hello sanchit

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
-  it('respond with hello tom', function(done) {
-    request(app).get('/').expect('hello tom', done);
+  it('respond with hello sanchit', function(done) {
+    request(app).get('/').expect('hello sanchit', done);
   });
 });
