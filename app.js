b43315beae59a1229e1b6df4da0a8889d0605a6f
mamtabhardwaj12 <mamta@cateina.com>
2018-04-24 19:23:10 +0530

    Switch to hello tom

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello cateina');
+  res.send('hello tom');
 });
  
 app.listen(process.env.PORT || 5000);
